9c676bc8fd689f4acee95b4f2e78cd6c368559c9
faharkhan <faharkhaan@gmail.com>
2024-05-30 20:29:52 +0500

    add food item, delete food item apis created

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
+export  const connectDB = async () =>{
     await mongoose.connect('mongodb+srv://faharkhaan:evolution@cluster0.vyds9bc.mongodb.net/food-delivery-app ').then(()=>console.log("DB Connected")); 
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js "
+import foodRouter from "./routes/food_routes.js"
 
 
 
@@ -15,13 +16,18 @@ app.use(cors())
 //db connection
 connectDB();
 
+//api endpoints
+app.use("/api/food", foodRouter)
+//for accessig images form the frontend
+app.use('/images', express.static('uploads'))  
+
 
 app.get("/", (req, res)=>{
     res.send("Api working")
 })
 
 app.listen(port, ()=>{
-    console.log('Server started on http://localhost:${port}')
+    console.log(`Server started on http://localhost:${port}`)
 })
 
  
